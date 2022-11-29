2f45510511fa9f1d9fd16fbf0c5da6c3d4b3bb32
Vishal <vishal.hall@kcl.ac.uk>
2022-11-29 21:23:45 +0000

    a:

--- things/forms.py
+++ things/forms.py
@@ -1,12 +1,16 @@
 """Forms of the project."""
 from django import forms
+from django.core.validators import MaxLengthValidator
 from .models import Thing
 # Create your forms here.
 
 class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
-        fields = ['name','description','quantity']
+        fields = ['name', 'description', 'quantity']
 
-    description = forms.Textarea
-    quantity = forms.NumberInput
+    description = forms.CharField(
+        widget=forms.Textarea(),
+        max_length=120
+        )
+    quantity = forms.NumberInput()
